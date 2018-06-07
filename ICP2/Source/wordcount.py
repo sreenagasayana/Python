79613088cc77c5880ecf86bd976f72e00a106c87
sreenagasayana <sreenagasayana@gmail.com>
2018-06-07 11:25:26 -0500

    update

--- ICP2/Source/wordcount.py
+++ ICP2/Source/wordcount.py
@@ -2,7 +2,11 @@ file = open(r"/Users/jayantis/Documents/Python/Python.txt", "r")
 
 content = file.read()
 words = content.split("\n")
+f = open('/Users/jayantis/Documents/Python/output.txt','w')
 for i in words:
-    print(i + "," + str(len(i)))
+    f.write(i + "," + str(len(i)))
+    f.write("\r")
+
+f.close()
 
 file.close()
