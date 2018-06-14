f2445b5031ada265f67b7c77b10357d17b3bde0b
sreenagasayana <sreenagasayana@gmail.com>
2018-06-14 11:01:54 -0500

    update

--- ICP4/Source/Array_ICP4.py
+++ ICP4/Source/Array_ICP4.py
@@ -1,8 +1,13 @@
 import numpy as np
 
 a = np.random.rand(10,10)
-Min =  a.min(),
+max_per_row = a.max(axis=1)
+min_per_row = a.min(axis=1)
+Min =  a.min()
 Max =  a.max()
+print("Array of elements")
 print(a)
-print("Minimum element in array:", Min)
-print("Maximum element in array:", Max)
+print("Maximum element in each row:")
+print(max_per_row)
+print("Minimum element in each row:")
+print(min_per_row)
