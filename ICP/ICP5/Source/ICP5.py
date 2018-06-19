7e382878b93555e4852c68ceaf5336edaa832d10
sreenagasayana <sreenagasayana@gmail.com>
2018-06-19 12:27:16 -0500

    update

--- ICP/ICP5/Source/ICP5.py
+++ ICP/ICP5/Source/ICP5.py
@@ -1,14 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import stats
 
-x = np.array([0,1,2,3,4,5,6,7,8,9])
-y = np.array([1,3,2,5,7,8,8,9,10,12])
-gradient, intercept, r_value, p_value, std_err = stats.linregress(x,y)
-mn=np.min(x)
-mx=np.max(x)
-x1=np.linspace(mn,mx,500)
-y1=gradient*x1+intercept
-plt.plot(x,y,'ob')
-plt.plot(x1,y1,'-r')
+xValues = np.array([0,1,2,3,4,5,6,7,8,9])
+yValues = np.array([1,3,2,5,7,8,8,9,10,12])
+
+#mean value of x and y
+meanOfX = xValues.mean()
+meanOfy = yValues.mean()
+
+#calculating slope
+slope = np.sum((xValues - meanOfX)*(yValues - meanOfy))/np.sum(np.square(xValues-meanOfX))
+
+#calculating intercept
+intercept = meanOfy - (slope * meanOfX)
+
+#y output
+yOutput = (slope * xValues) + intercept
+
+
+plt.plot(xValues,yValues)  # plotting the line made by linear regression
+
+plt.plot(xValues, yOutput)
 plt.show()
