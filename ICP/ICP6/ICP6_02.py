583f2363612f6fbe89c5f0493be615f4ce4400dc
sreenagasayana <sreenagasayana@gmail.com>
2018-06-21 11:56:24 -0500

    Update

--- ICP/ICP6/ICP6_02.py
+++ ICP/ICP6/ICP6_02.py
@@ -3,29 +3,26 @@ import pandas
 import pylab as pl
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
+#Loading CSV data
 variables = pandas.read_csv('/Users/jayantis/Desktop/stocks.csv')
 Y = variables[['returns']]
 X = variables[['dividendyield']]
-X_norm = (X - X.mean()) / (X.max() - X.min())
-Y_norm = (Y - Y.mean()) / (Y.max() - Y.min())
 Nc = range(1, 20)
 kmeans = [KMeans(n_clusters=i) for i in Nc]
-kmeans
 score = [kmeans[i].fit(Y).score(Y) for i in range(len(kmeans))]
-score
-#Second graph displaying the modified values of X and Y
+#Graph for the Nc and score calculated through CSV X and Y values.
 pl.plot(Nc,score)
 pl.xlabel('Number of Clusters')
 pl.ylabel('Score')
 pl.title('Elbow Curve')
 pl.show()
 pca = PCA(n_components=1).fit(Y)
+#Using PCA for transforming the X and Y points
 pca_d = pca.transform(Y)
 pca_c = pca.transform(X)
 #taking k as 3. As the first plot indicates the same
 kmeans=KMeans(n_clusters=3)
 kmeansoutput=kmeans.fit(Y)
-kmeansoutput
 #First graph plot displaying the data in the stock_data csv
 pl.figure('3 Cluster K-Means')
 pl.scatter(pca_c[:, 0], pca_d[:, 0], c=kmeansoutput.labels_)
